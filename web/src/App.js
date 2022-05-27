33b4623a17a48f839c78fd313d13889afaceda6b
FilipNikolov <nikolovfilip98@gmail.com>
2022-05-27 13:52:55 +0200

    init

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import Login from "./components/login";
 import { Nav } from "./components/Nav";
+
 
 export function App() {
   return (
     <div id="app">
       <Nav />
-      <Outlet />
+      <Login />
     </div>
   )
 }
